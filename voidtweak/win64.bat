db6883071707f46117c1a0bbb92380bcdd499a7f
NaKyle <nakyle.wright@gmail.com>
2023-11-19 05:38:19 -0600

    Refactor build script to dynamically find latest tools.

--- voidtweak/win64.bat
+++ voidtweak/win64.bat
@@ -1,97 +1,110 @@
 @echo off
-set PROJECT_DIR=voidtweak
-set PROJECT_EXE=voidtweak.exe
+
+set PROJECT_NAME=voidtweak
+set EXECUTABLE_NAME=%PROJECT_NAME%
+set INSTALLER_NAME=VoidTweakInstaller.exe
+set PACKAGE_NAME=com.nakyle.voidtweak
+set ZIPFILE_NAME=
+
+echo Looking up latest Qt version...
+set QT_PREFIX=C:\Qt
+set QT_SUFFIX=msvc2019_64\bin
+for /F "eol=| delims=" %%I in ('dir %QT_PREFIX%\6.* /AD /B /O-N 2^>nul') do (
+    if exist "%QT_PREFIX%\%%I\%QT_SUFFIX%\qmake.exe" (
+        set QT_BIN=%QT_PREFIX%\%%I\%QT_SUFFIX%
+        goto :FOUNDQT
+    )
+)
+goto :NOQTFOUND
+:FOUNDQT
+
+echo Looking up vcvarsall...
+set VC_PREFIX=C:\Program Files (x86)\Microsoft Visual Studio\2019
+set VC_SUFFIX=VC\Auxiliary\Build\vcvarsall.bat
+for /F "eol=| delims=" %%I in ('dir "%VC_PREFIX%\*" /AD /B /O-N 2^>nul') do (
+    if exist "%VC_PREFIX%\%%I\%VC_SUFFIX%" (
+        set VC_VARS="%VC_PREFIX%\%%I\%VC_SUFFIX%"
+        goto :FOUNDVC
+    )
+)
+goto :NOVCFOUND
+:FOUNDVC
+
+if "%INSTALLER_NAME%" == "" goto :FOUNDBC
+echo Looking up Qt Installer Framework...
+set BC_PREFIX=%QT_PREFIX%\Tools\QtInstallerFramework
+set BC_SUFFIX=bin
+for /F "eol=| delims=" %%I in ('dir "%BC_PREFIX%\*" /AD /B /O-N 2^>nul') do (
+    if exist "%BC_PREFIX%\%%I\%BC_SUFFIX%\binarycreator.exe" (
+        set BINARYCREATOR=%BC_PREFIX%\%%I\%BC_SUFFIX%\binarycreator.exe
+        goto :FOUNDBC
+    )
+)
+goto :NOBCFOUND
+:FOUNDBC
 
 set SCRIPT_DIR=%~dp0
-set VC_BIN=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build
-set QT_BIN=C:\Qt\6.3.2\msvc2019_64\bin
 set QT_TOOLCHAIN=%QT_BIN%\..\lib\cmake\Qt6\qt.toolchain.cmake
-set CMAKE_BIN=C:\Qt\Tools\CMake_64
-set QT_INSTALLER_BIN=C:\Qt\Tools\QtInstallerFramework\4.4\bin
-set PATH=%QT_BIN%;%CMAKE_BIN%;%QT_INSTALLER_BIN%;%PATH%
+set CMAKE_BIN=%QT_PREFIX%\Tools\CMake_64\bin
+set CMAKE=%CMAKE_BIN%\cmake.exe
+set PATH=%QT_BIN%;%CMAKE_BIN%;%PATH%
+
+echo Setting up MSVC environment...
+call %VC_VARS% x64
 
 echo Cleaning up...
-rmdir build-win64 /s /q  2> nul
+rmdir /s /q build-win64 2> nul
 mkdir build-win64
 cd build-win64
 
-echo Setting up MSVC environment...
-call "%VC_BIN%\vcvarsall.bat" x64
-
 echo Compiling...
-cmake -DCMAKE_TOOLCHAIN_FILE="%QT_TOOLCHAIN%" -DCMAKE_BUILD_TYPE=MinSizeRel ..\%PROJECT_DIR%
-cmake --build . --config MinSizeRel
+%CMAKE% -DCMAKE_TOOLCHAIN_FILE="%QT_TOOLCHAIN%" ^
+        -DCMAKE_BUILD_TYPE=MinSizeRel ^
+        ..\%PROJECT_NAME%
+%CMAKE% --build . --config MinSizeRel
 
 echo Pulling in dependencies...
-del /f /q MinSizeRel\*.pdb
-del /f /q MinSizeRel\*.lib
-del /f /q MinSizeRel\*.exp
-del /f /q MinSizeRel\*.manifest
-windeployqt.exe ^
+del /f /q MinSizeRel\*.pdb  2> nul
+del /f /q MinSizeRel\*.lib  2> nul
+del /f /q MinSizeRel\*.exp  2> nul
+del /f /q MinSizeRel\*.manifest  2> nul
+%QT_BIN%\windeployqt.exe ^
     --release ^
-    --no-compiler-runtime ^
-    --no-virtualkeyboard ^
-    --no-translations ^
-    --no-bluetooth ^
-    --no-concurrent ^
-    --no-designer ^
-    --no-designercomponents ^
-    --no-gamepad ^
-    --no-qthelp ^
-    --no-multimedia ^
-    --no-multimediawidgets ^
-    --no-multimediaquick ^
-    --no-nfc ^
-    --no-openglwidgets ^
-    --no-positioning ^
-    --no-printsupport ^
-    --no-qmltooling ^
-    --no-quickparticles ^
-    --no-quickwidgets ^
-    --no-script ^
-    --no-scripttools ^
-    --no-sensors ^
-    --no-serialport ^
-    --no-sql ^
-    --no-svgwidgets ^
-    --no-test ^
-    --no-widgets ^
-    --no-winextras ^
-    --no-webenginecore ^
-    --no-webengine ^
-    --no-webenginewidgets ^
-    --no-3dcore ^
-    --no-3drenderer ^
-    --no-3dquick ^
-    --no-3dquickrenderer ^
-    --no-3dinput ^
-    --no-3danimation ^
-    --no-3dextras ^
-    --no-geoservices ^
-    --no-webchannel ^
-    --no-texttospeech ^
-    --no-serialbus ^
-    --no-webview ^
-    --no-shadertools ^
-    --qmldir ..\%PROJECT_DIR% ^
-    MinSizeRel\%PROJECT_EXE%
-
-:: available qt components, can be excluded with --no-<name>
-::bluetooth concurrent core declarative designer designercomponents gamepad gui
-::qthelp multimedia multimediawidgets multimediaquick network nfc opengl
-::openglwidgets positioning printsupport qml qmltooling quick quickparticles
-::quickwidgets script scripttools sensors serialport sql svg svgwidgets test
-::websockets widgets winextras xml webenginecore webengine webenginewidgets 3dcore
-::3drenderer 3dquick 3dquickrenderer 3dinput 3danimation 3dextras geoservices
-::webchannel texttospeech serialbus webview
+    --qmldir ..\%PROJECT_NAME% ^
+    MinSizeRel\%EXECUTABLE_NAME%.exe
 
 cd /d %SCRIPT_DIR%
 
-echo Building installer...
-del /f /q installer\VoidTweakInstaller.exe
-rmdir /s /q installer\packages\com.nakyle.voidtweak\data
-mkdir installer\packages\com.nakyle.voidtweak\data
-robocopy build-win64\MinSizeRel installer\packages\com.nakyle.voidtweak\data /S /NFL /NDL /NJH /NJS /NP /NS /NC
-binarycreator --offline-only -c installer\config\config.xml -p installer\packages installer\VoidTweakInstaller.exe
+if not "%ZIPFILE_NAME%" == "" (
+    .\tools\7za.exe a -tzip %PROJECT_NAME%.zip .\build-win64\MinSizeRel\*
+)
+
+if not "%INSTALLER_NAME%" == "" (
+    echo Cleaning up installer folder...
+    del /f /q installer\%INSTALLER_NAME%  2> nul
+    rmdir /s /q installer\packages\%PACKAGE_NAME%\data  2> nul
+    mkdir installer\packages\%PACKAGE_NAME%\data
+    echo Moving in package data...
+    robocopy build-win64\MinSizeRel installer\packages\%PACKAGE_NAME%\data ^
+             /S /NFL /NDL /NJH /NJS /NP /NS /NC
+    echo Building installer...
+    %BINARYCREATOR% --offline-only ^
+                    -c installer\config\config.xml ^
+                    -p installer\packages ^
+                    installer\%INSTALLER_NAME%
+)
 
 echo Done!
+exit /b
+
+:NOQTFOUND
+echo No valid Qt install found!
+exit /b -1
+
+:NOVCFOUND
+echo No valid Visual C++ install found!
+exit /b -1
+
+:NOBCFOUND
+echo No valid Qt installer framework found!
+exit /b -1
